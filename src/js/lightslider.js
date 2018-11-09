cbb1face50ca617bba10661576a115a43bb3ec2d
Suresh shamala <suresh44t@gmail.com>
2018-11-09 20:56:54 +0530

    Fixed height issue

--- src/js/lightslider.js
+++ src/js/lightslider.js
@@ -368,7 +368,7 @@
                         }
                         var thumb = $children.eq(i * settings.slideMove).attr('data-thumb');
                         if (settings.gallery === true) {
-                            pagers += '<li style="width:100%;' + property + ':' + thumbWidth + 'px;' + gutter + ':' + settings.thumbMargin + 'px"><a href="#"><img src="' + thumb + '" /></a></li>';
+                            pagers += '<li style="width:100%;' + gutter + ':' + settings.thumbMargin + 'px"><a href="#"><img src="' + thumb + '" /></a></li>';
                         } else {
                             pagers += '<li><a href="#">' + (i + 1) + '</a></li>';
                         }
